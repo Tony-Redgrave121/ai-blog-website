85a88af1388113c69c2e35f6c90bd61da73e176d
Yulian Sribniak <153726291+Tony-Redgrave121@users.noreply.github.com>
2024-11-06 23:49:21 +0100

    Day 11

--- src/builds/desktop/components/main/information/Information.tsx
+++ src/builds/desktop/components/main/information/Information.tsx
@@ -33,48 +33,50 @@ const Information: React.FC<IBlogProps> = ({blog}) => {
                 <h1>{blog.title}</h1>
             </div>
             <div>
-                <CSSTransition nodeRef={contentBlock} in={blockState} timeout={0} classNames="content-block-node">
-                    <div ref={contentBlock} className={style.ContentBlock}>
-                        <div dangerouslySetInnerHTML={{__html: blog.content}}></div>
-                        <Button foo={() => handleBlockHeight()}>{blockState ?  <>Read Short Blog <HiArrowUp/></> : <>Read Full Blog <HiArrowDown/></>}</Button>
-                    </div>
-                </CSSTransition>
-                <div className={style.SideBar}>
-                    <div className={style.ReactionBlock}>
-                        <Button foo={() => {}} type={["PostButton"]}>{blog.likeState ? <HiHeart color="#FF5500"/> : <HiOutlineHeart/>} {formatCompact(blog.likeCount)}</Button>
-                        <Button foo={() => {}} type={["PostButton"]}><HiOutlineEye /> {formatCompact(blog.viewCount)}</Button>
-                        <Button foo={() => {}} type={["PostButton"]}><VscSend/> {formatCompact(blog.replyCount)}</Button>
-                    </div>
-                    <div>
-                        <section>
-                            <div>
-                                <p>Publication Date</p>
-                                <h4>{formatDate(blog.date)}</h4>
-                            </div>
-                            <div>
-                                <p>Category</p>
-                                <h4>{blog.category}</h4>
-                            </div>
-                            <div>
-                                <p>Reading Time</p>
-                                <h4>{Math.round(blog.readingTime / 100)} Min</h4>
-                            </div>
-                            <div>
-                                <p>Author Name</p>
-                                <h4>{blog.authorName}</h4>
-                            </div>
-                        </section>
+                <div>
+                    <CSSTransition nodeRef={contentBlock} in={blockState} timeout={0} classNames="content-block-node">
+                        <div ref={contentBlock} className={style.ContentBlock}>
+                            <div dangerouslySetInnerHTML={{__html: blog.content}}></div>
+                            <Button foo={() => handleBlockHeight()}>{blockState ?  <>Read Short Blog <HiArrowUp/></> : <>Read Full Blog <HiArrowDown/></>}</Button>
+                        </div>
+                    </CSSTransition>
+                    <div className={style.SideBar}>
+                        <div className={style.ReactionBlock}>
+                            <Button foo={() => {}} type={["PostButton"]}>{blog.likeState ? <HiHeart color="#FF5500"/> : <HiOutlineHeart/>} {formatCompact(blog.likeCount)}</Button>
+                            <Button foo={() => {}} type={["PostButton"]}><HiOutlineEye /> {formatCompact(blog.viewCount)}</Button>
+                            <Button foo={() => {}} type={["PostButton"]}><VscSend/> {formatCompact(blog.replyCount)}</Button>
+                        </div>
                         <div>
-                            <p>Table of Contents</p>
-                            <nav>
-                                <ul>
-                                    {
-                                        navigationLink.map(link => (
-                                            <li key={link}><a href={`#${link}`} onClick={handleLink}>{link}</a></li>
-                                        ))
-                                    }
-                                </ul>
-                            </nav>
+                            <section>
+                                <div>
+                                    <p>Publication Date</p>
+                                    <h4>{formatDate(blog.date)}</h4>
+                                </div>
+                                <div>
+                                    <p>Category</p>
+                                    <h4>{blog.category}</h4>
+                                </div>
+                                <div>
+                                    <p>Reading Time</p>
+                                    <h4>{Math.round(blog.readingTime / 100)} Min</h4>
+                                </div>
+                                <div>
+                                    <p>Author Name</p>
+                                    <h4>{blog.authorName}</h4>
+                                </div>
+                            </section>
+                            <div>
+                                <p>Table of Contents</p>
+                                <nav>
+                                    <ul>
+                                        {
+                                            navigationLink.map(link => (
+                                                <li key={link}><a href={`#${link}`} onClick={handleLink}>{link}</a></li>
+                                            ))
+                                        }
+                                    </ul>
+                                </nav>
+                            </div>
                         </div>
                     </div>
                 </div>
